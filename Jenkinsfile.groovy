a0949a0e5846a17fb1b06ba2064345e7c6e0845f
AKMAL KADYROV <AKMALKADYROV@AKMALs-MacBook-Pro.local>
2019-03-16 10:54:06 -0600

    2 properties added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,5 @@
 node {
-   properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please give an IP to host website', name: 'DEVIP', trim: true)])])
-   properties([pipelineTriggers([pollSCM('* * * * * ')])])
+   properties([parameters([string(defaultValue: '127.0.0.1', description: 'Please add IP address to host the website', name: 'DEVIP', trim: true)]), pipelineTriggers([pollSCM('* * * * * ')])])
  stage("Pull git") {
     git "git@github.com:akadyrov86/website.git"
  }
